0b7f0da9b0f5210d62ee18ee65786019d6037f40
Kibet <ronoisaiah537@gmail.com>
2025-06-16 09:35:33 +0300

    initial commit

---
+++ src/Calendar.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Calendar {
+
+
+    public static void printCalendar(int year , Month month){
+        LocalDate firstDay = LocalDate.of(year,month,1);
+        int lengthOfMonth = firstDay.lengthOfMonth();
+
+        System.out.println("\n\n" + month + " " + year);
+        System.out.println("SUN MON TUE WED THUR FRI SAT");
+
+        int startDay = firstDay.getDayOfWeek().getValue() %7;
+
+        for (int i =0; i<startDay; i++){
+            System.out.print("    ");
+        }
+        for (int day =1; day<=lengthOfMonth;day++){
+            System.out.printf("%3d ", day);
+
+            if ((startDay + day)%7 ==0){
+                System.out.println();
+            }
+        }
+System.out.println();
+    }
+    public static void main(String[] args) {
+int year = 2025;
+for (Month month : Month.values()){
+    printCalendar(year,month);
+}
+        }
+    }
